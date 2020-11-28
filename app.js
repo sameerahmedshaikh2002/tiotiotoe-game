c3211be421fa1be2ce592a716063af71a261a8d6
sameerahmedshaikh2002 <sameer2002ahmedshaikh@gmail.com>
2020-11-28 14:45:42 -0800

    sondition true

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ let sign = 'X';
 let firstPlayer = prompt("Type a first player");
 let secondPlayer = prompt("Type a second player");
 
+
+function calculate(value) {
+    let thisValue = document.getElementById("r" + value);
+    if (thisValue.innerText === "") {
+        thisValue.innerText = sign;
+        signChange();
+        checkWinner();
+
+    }
+}
+
+function signChange() {
+    if (sign === "X") {
+        sign = "O";
+    } else {
+        sign = "X";
+    }
+
+
+}
+
 let r1 = document.getElementById('r1');
 let r2 = document.getElementById('r2');
 let r3 = document.getElementById('r3');
@@ -12,24 +33,27 @@ let r7 = document.getElementById('r7');
 let r8 = document.getElementById('r8');
 let r9 = document.getElementById('r9');
 
-function calculate(value) {
-    let thisValue = document.getElementById("r" + value);
-    if (thisValue.innerText === "") {
-        thisValue.innerText = sign;
-        signChange();
+function checkWinner() {
+    if (r1.innerHTML === 'X' && r2.innerHTML === 'X' && r3.innerHTML === "X" ||
+        r1.innerHTML === 'X' && r4.innerHTML === 'X' && r7.innerHTML === "X" ||
+        r1.innerHTML === 'X' && r5.innerHTML === 'X' && r9.innerHTML === "X" ||
+        r4.innerHTML === 'X' && r5.innerHTML === 'X' && r6.innerHTML === "X" ||
+        r7.innerHTML === 'X' && r8.innerHTML === 'X' && r9.innerHTML === "X" ||
+        r7.innerHTML === 'X' && r8.innerHTML === 'X' && r9.innerHTML === "X" ||
+        r2.innerHTML === 'X' && r5.innerHTML === 'X' && r6.innerHTML === "X" ||
+        r3.innerHTML === 'X' && r6.innerHTML === 'X' && r9.innerHTML === "X"
+    ) {
+        alert(firstPlayer + ' ' + "is Winner");
+    }
+    else if (r1.innerHTML === 'O' && r2.innerHTML === 'O' && r3.innerHTML === "O" ||
+        r1.innerHTML === 'O' && r4.innerHTML === 'O' && r7.innerHTML === "O" ||
+        r1.innerHTML === 'O' && r5.innerHTML === 'O' && r9.innerHTML === "O" ||
+        r4.innerHTML === 'O' && r5.innerHTML === 'X' && r6.innerHTML === "O" ||
+        r7.innerHTML === 'O' && r8.innerHTML === 'O' && r9.innerHTML === "O" ||
+        r7.innerHTML === 'O' && r8.innerHTML === 'O' && r9.innerHTML === "O" ||
+        r2.innerHTML === 'O' && r5.innerHTML === 'O' && r6.innerHTML === "O" ||
+        r3.innerHTML === 'O' && r6.innerHTML === 'O' && r9.innerHTML === "O") {
+        alert(secondPlayer + ' ' + "is Winner")
     }
 }
 
-function signChange() {
-    if (sign === "X") {
-        sign = "O";
-    } else {
-        sign = "X";
-    }
-        
-
-}
-
-function champion() {
-
-}
